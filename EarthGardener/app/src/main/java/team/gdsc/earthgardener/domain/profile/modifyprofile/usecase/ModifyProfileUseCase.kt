601f0cd97bfe4b910e65840e547a795d61bc5d64
YuJungHyun <0703olivia@naver.com>
2022-05-19 17:47:27 +0900

    [FEAT] ModifyProfileUseCase 생성

---
+++ EarthGardener/app/src/main/java/team/gdsc/earthgardener/domain/profile/modifyprofile/usecase/ModifyProfileUseCase.kt
@@ -0,0 +1,13 @@
+package team.gdsc.earthgardener.domain.profile.modifyprofile.usecase
+
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import team.gdsc.earthgardener.domain.profile.modifyprofile.ModifyProfileData
+import team.gdsc.earthgardener.domain.profile.modifyprofile.ModifyProfileRepository
+
+class ModifyProfileUseCase(private val modifyProfileRepository: ModifyProfileRepository) {
+    suspend operator fun invoke(image: MultipartBody.Part,data: HashMap<String, RequestBody>):
+            ModifyProfileData{
+        return modifyProfileRepository.putModifyProfileResult(image, data)
+    }
+}
